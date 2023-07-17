ad9133047646403956c0f2c45b09d19acb3018c0
Cory Fabian <Coryf65@users.noreply.github.com>
2023-07-17 15:46:24 -0500

    Encapsulation
    Make a property with a backing field

--- Assets/Scripts/ResourcePile.cs
+++ Assets/Scripts/ResourcePile.cs
@@ -9,9 +9,13 @@ using UnityEngine;
 public class ResourcePile : Building
 {
     public ResourceItem Item;
+    public float ProductionSpeed
+    {
+        get { return _productionSpeed; } // getter returns backing field
+        set { _productionSpeed = value; } // setter uses backing field
+    }
 
-    public float ProductionSpeed = 0.5f;
-
+    private float _productionSpeed = 0.5f;
     private float m_CurrentProduction = 0.0f;
 
     private void Update()
@@ -26,13 +30,13 @@ public class ResourcePile : Building
         
         if (m_CurrentProduction < 1.0f)
         {
-            m_CurrentProduction += ProductionSpeed * Time.deltaTime;
+            m_CurrentProduction += _productionSpeed * Time.deltaTime;
         }
     }
 
     public override string GetData()
     {
-        return $"Producing at the speed of {ProductionSpeed}/s";
+        return $"Producing at the speed of {_productionSpeed}/s";
         
     }
     
